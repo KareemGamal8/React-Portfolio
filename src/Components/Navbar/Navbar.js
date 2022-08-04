e678154f3dd68d262a4010a02d3b500c5cf4fb22
KareemGamal8 <108495466+KareemGamal8@users.noreply.github.com>
2022-08-04 23:52:12 +0200

    Update Navbar.js

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import './Navbar.css'
-import Logo from '../images/logo-symbol-dark.png'
+import React from 'react';
+import './Navbar.css';
+import {BrowserRouter,NavLink} from 'react-router-dom';
+import Logo from '../images/logo-symbol-dark.png';
 function Navbar() {
 
     return (
@@ -9,12 +10,14 @@ function Navbar() {
                     <div className='navbar-links'>
                         <img src={Logo} alt='Logo' className='logo'/>
                         <div className='links'>
-                            <a href="/">Home <span></span></a>
-                            <a href="/">About <span></span></a>
-                            <a href="/">Services <span></span></a>
-                            <a href="/">Portfolio <span></span></a>
-                            <a href="/">Blog <span></span></a>
-                            <a href="/">Contact <span></span></a>
+                           <BrowserRouter>
+                                <NavLink to="/">Home <span></span></NavLink>
+                                <NavLink to="/About">About <span></span></NavLink>
+                                <NavLink to="/Services">Services <span></span></NavLink>
+                                <NavLink to="/Portfolio">Portfolio <span></span></NavLink>
+                                <NavLink to="/Blog">Blog <span></span></NavLink>
+                                <NavLink to="/Contact">Contact <span></span></NavLink>
+                           </BrowserRouter>
                         </div>
                         </div>
                         <div className='navbar-icons'>
