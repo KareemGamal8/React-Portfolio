9083658de6c1efc7cdd05626bbb2764acb9dd011
KareemGamal8 <108495466+KareemGamal8@users.noreply.github.com>
2022-08-05 21:07:28 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Services from './Components/Services/Services';
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+      <Services />
+    </div>
+  );
+}
+
+export default App;
