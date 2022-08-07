4302b999f5e01afd6fe8134e7c52c8e8b9104ace
KareemGamal8 <108495466+KareemGamal8@users.noreply.github.com>
2022-08-07 02:58:27 +0200

    Add files via upload

---
+++ src/Components/About/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './About.css'
+function Main() {
+  return (
+    <div className='main'>
+        <h4>User experience design - UI/UX</h4>
+        <p>Delight the user and make it work.</p>
+        <h4>Web and user interface design - Development</h4>
+        <p>Websites, web experiences, ...</p>
+        <h4>Interaction design - Animation</h4>
+        <p>I like to move it move it.</p>
+    </div>
+  )
+}
+
+export default Main
