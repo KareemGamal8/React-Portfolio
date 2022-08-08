7195f8fbc71da5efa4be2c1f5831dff43bf17363
KareemGamal8 <108495466+KareemGamal8@users.noreply.github.com>
2022-08-08 22:56:24 +0200

    Add files via upload

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Footer.css";
+import logoImage from "../images/logo-symbol-dark.png";
+function Footer() {
+  return (
+    <div className="footer pd-x">
+      <div className="footer-sections">
+        <div className="footer-left">
+          <img src={logoImage} alt="logo" />
+          <h2>trydo</h2>
+        </div>
+        <div>hghg</div>
+        <div className="footer-center">
+          <i className="fa-brands fa-facebook-f"></i>
+          <i className="fa-brands fa-twitter"></i>
+          <i className="fa-brands fa-linkedin-in"></i>
+          <i className="fa-brands fa-instagram"></i>
+        </div>
+        <div className="footer-right">
+          <p>© 2022. All rights reserved by Rainbow-Themes.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from 'react';
+import './Navbar.css';
+import Logo from '../images/logo-symbol-dark.png';
+function Navbar() {
+    const [navScroll, setNavScroll] = useState(false);
+    const [navHandler, setNavHandler] = useState(true)
+    const btnHandler = () => setNavHandler(!navHandler); 
+    useEffect(() => {
+        const changeBackground = () => {
+            if (window.scrollY >= 500) {
+                setNavScroll(true);
+            } else {
+                setNavScroll(false);
+            }
+        }
+        window.addEventListener("scroll", changeBackground);
+    })
+    return (
+        <div className={navScroll ? "nav-scroll" : "navbar"}>
+                <div className='navbar-content pd-x'>
+                    <div className='navbar-links'>
+                        <img src={Logo} alt='Logo' className='logo'/>
+                        <div className={navHandler ? "links" : "links-hidden"}>
+                                <ul>
+                                    <li><a href='#Home'>Home<span></span></a></li>
+                                    <li><a href='#About'>About <span></span></a></li>
+                                    <li><a href='#Services'>Services <span></span></a></li>
+                                    <li><a href='#Portfolio'>Portfolio <span></span></a></li>
+                                    <li><a href='#Blog'>Blog <span></span></a></li>
+                                    <li><a href='#Contact'>Contact <span></span></a></li>
+                                </ul>
+                        </div>
+                    </div>
+                        <div className='navbar-icons'>
+                            <i className="fa-brands fa-facebook-f"></i>
+                            <i className="fa-brands fa-twitter"></i>
+                            <i className="fa-brands fa-linkedin-in"></i>
+                            <i className="fa-brands fa-instagram"></i>
+                        </div>
+                            <div className='navbar-bars' >
+                                <i className="fa-solid fa-bars" onClick={btnHandler}></i>
+                            </div>
+                </div>
+        </div>
+    )
+}
+
+export default Navbar;
