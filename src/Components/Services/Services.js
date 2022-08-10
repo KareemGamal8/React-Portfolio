b8ce282dab70d0b68e33a11eff9e8d065d9bb515
KareemGamal8 <108495466+KareemGamal8@users.noreply.github.com>
2022-08-10 22:44:07 +0200

    Update Services.js

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,59 +1,51 @@
-import React from 'react'
-import './Services.css'
-import Cards from './Cards'
+import React, { useState } from "react";
+import "./Services.css";
+import Cards from "./Cards";
 function Services() {
+  const [cardLIst, setCardList] = useState(
+    {
+      icon: "fa-solid fa-layer-group",
+      title: "web development",
+      desc: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered.",
+    },
+    {
+      icon: "fa-solid fa-layer-group",
+      title: "web development",
+      desc: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered.",
+    },
+    {
+      icon: "fa-solid fa-layer-group",
+      title: "web development",
+      desc: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered.",
+    },
+    {
+      icon: "fa-solid fa-layer-group",
+      title: "web development",
+      desc: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered.",
+    },
+    {
+      icon: "fa-solid fa-layer-group",
+      title: "web development",
+      desc: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered.",
+    },
+    {
+      icon: "fa-solid fa-layer-group",
+      title: "web development",
+      desc: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered.",
+    }
+  );
   return (
-    <div className='services pd-x' id="Services">
-      <div className='pd-y'>
-        <h4 className='section-title'>My Awesome Service</h4>
-        <p className='section-desc'>There are many variations of passages of Lorem Ipsum available, but <br/> the majority have suffered alteration.</p>
-        <div className='cards'>
-            <div className='card'>  
-              <Cards 
-                icon="fa-solid fa-layer-group" 
-                title="web development" 
-                desc="There are many variations of passages of Lorem Ipsum available, but the majority have suffered." 
-                />
-            </div>
-            <div className='card'>  
-              <Cards 
-                icon="fa-solid fa-layer-group" 
-                title="web development" 
-                desc="There are many variations of passages of Lorem Ipsum available, but the majority have suffered." 
-                />
-            </div>
-            <div className='card'>  
-              <Cards 
-                icon="fa-solid fa-layer-group" 
-                title="web development" 
-                desc="There are many variations of passages of Lorem Ipsum available, but the majority have suffered." 
-                />
-            </div>
-            <div className='card'>  
-              <Cards 
-                icon="fa-solid fa-layer-group" 
-                title="web development" 
-                desc="There are many variations of passages of Lorem Ipsum available, but the majority have suffered." 
-                />
-            </div>
-            <div className='card'>  
-              <Cards 
-                icon="fa-solid fa-layer-group" 
-                title="web development" 
-                desc="There are many variations of passages of Lorem Ipsum available, but the majority have suffered." 
-                />
-            </div>
-            <div className='card'>  
-              <Cards 
-                icon="fa-solid fa-layer-group" 
-                title="web development" 
-                desc="There are many variations of passages of Lorem Ipsum available, but the majority have suffered." 
-                />
-            </div>
-        </div>
+    <div className="services pd-x" id="Services">
+      <div className="pd-y">
+        <h4 className="section-title">My Awesome Service</h4>
+        <p className="section-desc">
+          There are many variations of passages of Lorem Ipsum available, but{" "}
+          <br /> the majority have suffered alteration.
+        </p>
+        <Cards cardLIst={cardLIst} />
       </div>
     </div>
-  )
+  );
 }
 
-export default Services
+export default Services;
