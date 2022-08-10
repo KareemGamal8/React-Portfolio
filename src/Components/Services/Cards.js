efe82bb1fb6cef4a581842904dfbc358183bfd74
KareemGamal8 <108495466+KareemGamal8@users.noreply.github.com>
2022-08-10 22:44:47 +0200

    Update Cards.js

--- src/Components/Services/Cards.js
+++ src/Components/Services/Cards.js
@@ -1,11 +1,12 @@
-import React, { Fragment } from 'react'
-import './Services.css'
-export default function Cards({title, desc, icon}) {
-  return (
-    <Fragment>
-        <i className={icon}></i>
-        <div className='card-title'>{title}</div>
-        <div className='card-desc'>{desc}</div>
-    </Fragment>
-  )
+import React from "react";
+import "./Services.css";
+export default function Cards({ cardLIst }) {
+  const card = cardLIst.map(({ icon, title, desc }, idx) => {
+    <div className="card" key={idx}>
+      <i className={icon}></i>
+      <div className="card-title">{title}</div>
+      <div className="card-desc">{desc}</div>
+    </div>;
+  });
+  return <div>{card}</div>;
 }
